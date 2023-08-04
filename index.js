9761ba7152e6ffbd12d9f3ba3917298ae9f1506d
obiwan-overflow <kittipopp@hotmail.co.th>
2023-08-04 11:30:10 +0700

    update

--- index.js
+++ index.js
@@ -2108,6 +2108,6 @@ app.post('/news', (req, res) => {
   }
 });
 // ****************************************************************************************************************** End news
-app.listen(3003, () => {
-  console.log('Server started on port 3003');
+app.listen(3500, () => {
+  console.log('Server started on port 3500');
 });
